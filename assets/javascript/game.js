e34841b49fc8e35b3f992d167361920d901596e9
Bob-Zombie <bob.almanzar@gmail.com>
2019-05-18 02:41:45 -0700

    added crystal images, basic random number generation for crystals, some styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,32 @@
 //Functions Needed
+
 for (var i = 0; i < 4; i++) {
-    console.log("hola");
+
+    var random = Math.floor(Math.random() * 12);
+    console.log(random)
+    var crystal = $("<div>");
+    crystal.attr("class", 'crystal');
+
+    $(".fourCrystals").append(crystal);
+    console.log("Hola again");
 }
+
+var randomNum;
+var loss;
+var win;
+
+
+
+
+
+
+
+//FUNCTIONS NEEDED-->
+//Declare variables needed-->
+//Main Number: var randomNum;-->
+//Crystal Number 1: var randomNum;-->
+//Crystal Number 2: var randomNum;-->
+//Crystal Number 3: var randomNum;-->
+//Crystal Number 4: var randomNum;-->
+//Track Loss: var loss;-->
+//Track Win: var win-->
